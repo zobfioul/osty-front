183b8acb10f3f0cc21ba89866396398e0108ef64
arthur-arctos <arthur@arctos-ingenierie.fr>
2023-03-14 05:45:07 +0100

    add dynamic sidebar and logout page

---
+++ components/SideBarElement.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: ['title', 'url']
+}
+</script>
+
+<template>
+    <li>
+        <NuxtLink v-bind:to="url" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <slot></slot>
+            <span class="ml-3">{{ title }}</span>
+        </NuxtLink>
+    </li>
+</template>
---
+++ pages/logout.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        setup() {
+            // TODO : actually logout the user ( delete cookies, etc )
+
+            const router = useRouter()
+            router.push({
+                path: '/login',
+            })
+        }
+    }
+</script>
+
+
+
+<template>
+
+</template>
