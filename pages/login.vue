997346943c364fb729fcce1d7e946b6bc4433a82
arthur-arctos <arthur@arctos-ingenierie.fr>
2023-03-14 10:51:37 +0100

    add signup page

--- pages/login.vue
+++ pages/login.vue
@@ -3,7 +3,7 @@
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <a href="#" class="flex items-center mb-12 text-5xl font-semibold text-black dark:text-white font-mono">
                 <img class="w-14 mr-2" src="https://dieselpunkcore.com/wp-content/uploads/2014/06/logo-placeholder.png" alt="logo">
-                ostie
+                Osty
             </a>
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
