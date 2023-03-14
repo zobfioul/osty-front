7f369b648e002ddb76a71a06b3afceb53e70508b
arthur-arctos <arthur@arctos-ingenierie.fr>
2023-03-14 10:55:09 +0100

    add forgotten password page

--- pages/forgot-password.vue
+++ pages/forgot-password.vue
@@ -1,7 +1,24 @@
 <template>
-    <div>
-        <h1 class="text-3xl font-bold">
-        Password Forgot page : under construction
-        </h1>
-    </div>
+    <section class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('~/assets/images/login-background.jpg')]">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a href="#" class="flex items-center mb-12 text-5xl font-semibold text-black dark:text-white font-mono">
+                <img class="w-14 mr-2" src="https://dieselpunkcore.com/wp-content/uploads/2014/06/logo-placeholder.png" alt="logo">
+                Osty
+            </a>
+            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Forgoten password
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="http://localhost:3000/home">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                        </div>
+                        <button type="submit" class="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Sign up</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
