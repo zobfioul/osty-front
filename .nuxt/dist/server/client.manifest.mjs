2d24ec732cbe7c139b88166597cbc57340d779c5
arthur-arctos <arthur@arctos-ingenierie.fr>
2023-03-15 01:08:13 +0100

    add gitignore

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,15 @@
-export { default } from "file:///home/arthur/Projects/Perso/osty-front/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export default {
+  "@vite/client": {
+    "isEntry": true,
+    "file": "@vite/client",
+    "css": [],
+    "module": true,
+    "resourceType": "script"
+  },
+  "/home/arthur/Projects/Perso/osty-front/node_modules/nuxt/dist/app/entry.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "isEntry": true,
+    "file": "/home/arthur/Projects/Perso/osty-front/node_modules/nuxt/dist/app/entry.mjs"
+  }
+}
